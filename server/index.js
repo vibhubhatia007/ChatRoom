f122692b7a0edea2d5ae1df8a22eaa3ec48853bd
Vibhu Bhatia <vibhubhatia0007@gmail.com>
2021-02-27 01:31:13 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const PORT =  process.env.PORT || 5000 ;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+server.listen(PORT, ()=> console.log(`server has started on port ${PORT}`));
+
